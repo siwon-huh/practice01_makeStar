8207b2c6b4169901a82620af7ae3fd67bd55f2b2
siwon-huh <shewon.heo@dream-academy.io>
2023-03-12 15:52:28 +0900

    initial commit

--- src/Pyramid.sol
+++ src/Pyramid.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-
+import "forge-std/console.sol";
 contract Pyramid {
-    function run(uint size) pure public returns (string memory _out) {
-        _out = ""; // TODO
-        /* Example (size = 5):
-         *
-         *     "*\n"
-         *     "**\n"
-         *     "***\n"
-         *     "****\n"
-         *     "*****\n"
-         *
-         */
+    function run(uint size) view public returns (string memory _out) {
+        _out = "";
+        for (uint i = 0; i < size; i++){
+            for (uint j = 0; j <= i; j++){
+                _out = string.concat(_out, "*");
+            }
+            _out= string.concat(_out, "\n");
+        }
+        console.log("%s", _out);
     }
 }
